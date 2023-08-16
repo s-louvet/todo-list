99718fad8143d381a30ba571e81cd93cf0758844
Sylvain Louvet <slouvet@cutii.io>
2023-08-16 19:03:01 +0200

    style: refacto a bit entry function

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,34 @@ use tezos_smart_rollup::storage::path::OwnedPath;
 use tezos_smart_rollup::{kernel_entry, prelude::Runtime};
 
 pub fn entry<Host: Runtime>(host: &mut Host) {
-    //Read next id to know if we will have to write new todo in storage
     let todo_list_next_id_path: OwnedPath =
         "/todo_list/next-id".as_bytes().to_vec().try_into().unwrap();
-    let next_id = host
-        .store_read(&todo_list_next_id_path, 0, 8)
+
+    let next_id = load_next_id(host, &todo_list_next_id_path);
+    let todo_list = load_todo_list(host, &next_id);
+
+    host.write_debug("Hello kernel\n");
+    let new_todo_list = execute(host, todo_list.clone());
+
+    save_todo_list(
+        host,
+        new_todo_list,
+        todo_list,
+        &todo_list_next_id_path,
+        &next_id,
+    );
+}
+
+fn load_next_id<Host: Runtime>(host: &mut Host, todo_list_next_id_path: &OwnedPath) -> Id {
+    //Read next id to know if we will have to write new todo in storage
+
+    host.store_read(todo_list_next_id_path, 0, 8)
         .map_err(|_| "Runtime error".to_string())
         .and_then(Id::try_from)
-        .unwrap_or_default();
+        .unwrap_or_default()
+}
+
+fn load_todo_list<Host: Runtime>(host: &mut Host, next_id: &Id) -> TodoList {
     let mut todo_list = TodoList::default();
     if next_id.id != 0 {
         for i in 0..next_id.id {
@@ -31,18 +51,20 @@ pub fn entry<Host: Runtime>(host: &mut Host) {
             todo_list.todo_list.insert(i, todo);
         }
     }
+    todo_list
+}
 
-    host.write_debug("Hello kernel\n");
-    let new_todo_list = execute(host, todo_list.clone());
-
+fn save_todo_list<Host: Runtime>(
+    host: &mut Host,
+    new_todo_list: TodoList,
+    old_todo_list: TodoList,
+    todo_list_next_id_path: &OwnedPath,
+    next_id: &Id,
+) {
     //If there is new todo to the original list
-    if new_todo_list.todo_list.len() > todo_list.todo_list.len() {
+    if new_todo_list.todo_list.len() > old_todo_list.todo_list.len() {
         //Write the next id in the storage to know which id to add next time
-        let _ = host.store_write(
-            &todo_list_next_id_path,
-            &((next_id.id + 1).to_be_bytes()),
-            0,
-        );
+        let _ = host.store_write(todo_list_next_id_path, &((next_id.id + 1).to_be_bytes()), 0);
         //Write the new todo in the storage
         let todo_path: OwnedPath = format!("/todo_list/{}", next_id.id)
             .as_bytes()
@@ -53,8 +75,8 @@ pub fn entry<Host: Runtime>(host: &mut Host) {
         let _ = host.store_write(&todo_path, &todo, 0);
     }
 
-    if new_todo_list.todo_list != todo_list.todo_list {
-        for (id, todo) in &todo_list.todo_list {
+    if new_todo_list.todo_list != old_todo_list.todo_list {
+        for (id, todo) in &old_todo_list.todo_list {
             if new_todo_list.todo_list[id] != *todo {
                 //Write the new status of the todo in the storage
                 let todo_path: OwnedPath = format!("/todo_list/{}", id)
@@ -68,7 +90,6 @@ pub fn entry<Host: Runtime>(host: &mut Host) {
         }
     }
 }
-
 kernel_entry!(entry);
 
 fn execute<Host: Runtime>(host: &mut Host, todo_list: TodoList) -> TodoList {
